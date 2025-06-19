6573c7d388218f5893666d3eab2ee32f9f8434e9
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-06-19 11:29:04 -0700

    adding utility to load 2d FNO

--- operator_aliasing/models/utils.py
+++ operator_aliasing/models/utils.py
@@ -4,9 +4,25 @@ from __future__ import annotations
 
 import typing
 
+from neuralop.models import FNO
 from torch.nn import Module
 
 
 def get_model(model_name: str, **model_args: typing.Any) -> Module:
     """Get model w/ args."""
-    return None
+    model = None
+    max_modes = model_args['max_modes']
+    hidden_channels = model_args['hidden_channels']
+    in_channels = model_args['in_channels']
+    out_channels = model_args['out_channels']
+
+    if model_name == 'FNO2D':
+        starting_modes = (max_modes, max_modes)
+        model = FNO(
+            n_modes=starting_modes,
+            hidden_channels=hidden_channels,
+            in_channels=in_channels,
+            out_channels=out_channels,
+        )
+
+    return model
