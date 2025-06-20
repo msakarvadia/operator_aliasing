55116f6ff2e127bbc5b0d1150a7b90b32c036f3f
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-06-20 10:53:34 -0700

    adding ckpt saving function

--- operator_aliasing/train/utils.py
+++ operator_aliasing/train/utils.py
@@ -2,6 +2,10 @@
 
 from __future__ import annotations
 
+import os
+import typing
+
+import torch
 from torch import nn
 
 
@@ -11,3 +15,14 @@ def get_loss(loss_name: str) -> nn.Module:
     if loss_name == 'l1':
         loss = nn.L1Loss()
     return loss
+
+
+def save_ckpt(ckpt_path: str, ckpt_dict: typing.Any) -> None:
+    """Ckpt model during training."""
+    if not os.path.exists(ckpt_path):
+        os.makedirs(ckpt_path)
+    ckpt_num = ckpt_dict['epoch']
+    torch.save(ckpt_dict, f'{ckpt_path}/{ckpt_num}_ckpt.pth')
+
+
+# def load_latest_ckpt(ckpt_path:str) -> :
