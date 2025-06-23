c7eadf87de4981e389eaec5d33187a9c0dc3236a
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-06-23 14:44:26 -0700

    adding more comprehensive experiment for super-resolution

--- experiments/training.py
+++ experiments/training.py
@@ -16,9 +16,13 @@ def hello_world() -> str:
 
 
 @bash_app
-def train() -> str:
+def train(filter_lim: int, downsample_dim: int) -> str:
     """Train a model."""
-    exec_str = 'pwd; python main.py'
+    exec_str = f"""pwd;
+    python main.py --filter_lim {filter_lim} \n
+    --downsample_dim {downsample_dim} \n
+    --ckpt_path ckpts/{downsample_dim}_{filter_lim}/
+    """
     return exec_str
 
 
@@ -26,6 +30,17 @@ if __name__ == '__main__':
     config = get_parsl_config()
     parsl.load(config)
 
-    future = train()
-    print(f'Waiting for {future}')
-    print(f'Got result {future.result()}')
+    train_args = []
+    for downsample_dim in [-1, 8, 11]:
+        for filter_lim in [-1, 5]:
+            # don't downsample unfiltered data
+            if filter_lim == -1 and downsample_dim != -1:
+                continue
+            train_args.append(
+                {'downsample_dim': downsample_dim, 'filter_lim': filter_lim}
+            )
+
+    futures = [train(**args) for args in train_args]
+    for future in futures:
+        print(f'Waiting for {future}')
+        print(f'Got result {future.result()}')
