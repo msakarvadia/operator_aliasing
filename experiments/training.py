4b50d5d5b2ed4ec9e2564a81a8486ac8d227a2a1
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-07-03 13:42:44 -0700

    attempting to add HP search

--- experiments/training.py
+++ experiments/training.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import argparse
+import typing
 
 import parsl
 from parsl.app.app import bash_app
@@ -18,35 +19,73 @@ def hello_world() -> str:
 
 
 @bash_app
-def train(dataset_name: str, filter_lim: int, downsample_dim: int) -> str:
+def train(**kwargs: typing.Any) -> str:
     """Train a model."""
+    filter_lim = kwargs['filter_lim']
+    downsample_dim = kwargs['downsample_dim']
+    dataset_name = kwargs['dataset_name']
+    lr = kwargs['lr']
+    wd = kwargs['weight_decay']
+    step_size = kwargs['step_size']
+    gamma = kwargs['gamma']
+
+    arg_path = '_'.join(map(str, list(kwargs.values())))
+    # Need to remove any . or / to
+    # ensure a single continuous file path
+    arg_path = arg_path.replace('.', '')
+    ckpt_name = arg_path.replace('/', '')
+
     if dataset_name == 'darcy':
         exec_str = f"""pwd;
         python main.py --filter_lim {filter_lim} \
         --downsample_dim {downsample_dim} \
-        --ckpt_path ckpts/{downsample_dim}_{filter_lim} \
+        --lr {lr} \
+        --weight_decay {wd} \
+        --step_size {step_size} \
+        --gamma {gamma} \
+        --ckpt_path ckpts/{ckpt_name} \
         """
     if dataset_name == 'darcy_pdebench':
         exec_str = f"""pwd;
         python main.py --filter_lim {filter_lim} \
         --downsample_dim {downsample_dim} \
+        --lr {lr} \
+        --weight_decay {wd} \
+        --step_size {step_size} \
+        --gamma {gamma} \
         --dataset_name darcy_pdebench \
         --img_size 128 \
-        --ckpt_path darcy_pdebench_ckpts/{downsample_dim}_{filter_lim} \
+        --ckpt_path darcy_pdebench_ckpts/{ckpt_name} \
         """
     return exec_str
 
 
 @bash_app
-def train_w_pinn_loss(
-    dataset_name: str, filter_lim: int, downsample_dim: int
-) -> str:
-    """Train a model."""
+def train_w_pinn_loss(**kwargs: typing.Any) -> str:
+    """Train a model w/ PINN loss."""
+    filter_lim = kwargs['filter_lim']
+    downsample_dim = kwargs['downsample_dim']
+    dataset_name = kwargs['dataset_name']
+    lr = kwargs['lr']
+    wd = kwargs['weight_decay']
+    step_size = kwargs['step_size']
+    gamma = kwargs['gamma']
+
+    arg_path = '_'.join(map(str, list(kwargs.values())))
+    # Need to remove any . or / to
+    # ensure a single continuous file path
+    arg_path = arg_path.replace('.', '')
+    ckpt_name = arg_path.replace('/', '')
+
     if dataset_name == 'darcy':
         exec_str = f"""pwd;
         python main.py --filter_lim {filter_lim} \
         --downsample_dim {downsample_dim} \
-        --ckpt_path ckpts/pinn_loss/{downsample_dim}_{filter_lim} \
+        --lr {lr} \
+        --weight_decay {wd} \
+        --step_size {step_size} \
+        --gamma {gamma} \
+        --ckpt_path ckpts/pinn_loss/{ckpt_name} \
         --loss_name darcy_pinn \
         """
     if dataset_name == 'darcy_pdebench':
@@ -55,8 +94,12 @@ def train_w_pinn_loss(
         python main.py --filter_lim {filter_lim} \
         --downsample_dim {downsample_dim} \
         --dataset_name darcy_pdebench \
+        --lr {lr} \
+        --weight_decay {wd} \
+        --step_size {step_size} \
+        --gamma {gamma} \
         --img_size 128 \
-        --ckpt_path {ckpt_path}/{downsample_dim}_{filter_lim} \
+        --ckpt_path {ckpt_path}/{ckpt_name} \
         --loss_name darcy_pinn \
         """
     return exec_str
@@ -130,6 +173,20 @@ if __name__ == '__main__':
                 'filter_lim': filter_lim,
             }
             train_args.append(training_args)
+
+    # Add hyper-parameter search:
+    hyper_param_search_args = []
+    for train_arg in train_args:
+        for lr in [1e-2, 1e-3, 1e-5]:
+            for wd in [1e-6, 1e-7, 1e-8, 1e-9]:
+                for step_size in [15, 20, 25]:
+                    for gamma in [0.25, 0.5, 0.1]:
+                        exp_arg = train_arg.copy()
+                        exp_arg['lr'] = lr
+                        exp_arg['weight_decay'] = wd
+                        exp_arg['step_size'] = step_size
+                        exp_arg['gamma'] = gamma
+                        hyper_param_search_args.append(exp_arg)
 
     config = get_parsl_config(
         walltime=args.walltime, queue=args.queue, num_nodes=args.num_nodes
