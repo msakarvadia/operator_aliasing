6374d4cd58d41c217ad583ab2b03c2b7e1ecda7d
Mansi Sakarvadia <sakarvadia@uchicago.edu>
2025-09-11 17:50:27 -0700

    adding support to training CNO2d

--- operator_aliasing/models/utils.py
+++ operator_aliasing/models/utils.py
@@ -7,7 +7,7 @@ import typing
 from neuralop.models import FNO
 from torch.nn import Module
 
-from operator_aliasing.models.cno2d import CNO2d
+from operator_aliasing.models.CNO2d_original_version.CNOModule import CNO
 from operator_aliasing.models.crop2d import CROPFNO2d
 
 
@@ -20,22 +20,36 @@ def get_model(**model_args: typing.Any) -> Module:
     in_channels = model_args['in_channels']
     out_channels = model_args['out_channels']
 
-    # crop specific params
-    # in_size = model_args['img_size']
+    # crop + CNO specific params
     latent_size = model_args['latent_size']
 
+    # CNO params
+    in_size = model_args['img_size']
+
     if model_name == 'CNO2D':
-        # NOTE (MS): default recommended from
-        # original repo
-        # https://github.com/camlab-ethz/ConvolutionalNeuralOperator/blob/main/CNO2d_vanilla_torch_version/CNO2d.py#L203 # noqa
-        model = CNO2d(
-            in_dim=in_channels,
-            out_dim=out_channels,
-            n_layers=1,
-            n_res=4,
-            n_res_neck=4,
-            channel_multiplier=hidden_channels,
-            use_bn=True,
+        # defaults from
+        # https://github.com/camlab-ethz/ConvolutionalNeuralOperator/blob/main/CNO2d_original_version/CNOModule.py#L261 # noqa
+        model = CNO(
+            in_dim=in_channels,  # Number of input channels.
+            in_size=in_size,  # Input spatial size
+            N_layers=1,  # Number of (D) or (U) blocks in the network
+            N_res=1,  # Number of (R) blocks per level (except the neck)
+            N_res_neck=6,  # Number of (R) blocks in the neck
+            channel_multiplier=32,  # How the number of channels evolve?
+            conv_kernel=3,  # Size of all the kernels
+            cutoff_den=2.0001,  # Filter property 1.
+            filter_size=6,  # Filter property 2.
+            lrelu_upsampling=2,  # Filter property 3.
+            half_width_mult=0.8,  # Filter property 4.
+            radial=False,  # Filter property 5. Is filter radial?
+            batch_norm=True,  # Add BN? We do not add BN in lifting/projection layer # noqa
+            out_dim=out_channels,  # Target dimension
+            out_size=1,  # If out_size is 1, Then out_size = in_size. Else must be int # noqa
+            expand_input=False,  # Start with original in_size, or expand it (pad zeros in the spectrum) # noqa
+            latent_lift_proj_dim=latent_size,  # Intermediate latent dimension in the lifting/projection layer # noqa
+            add_inv=True,  # Add invariant block (I) after the intermediate connections? # noqa
+            activation='cno_lrelu_torch',  # Activation function can be 'cno_lrelu' or 'lrelu' # noqa
+            # activation='lrelu'
         )
     if model_name == 'CROP2D':
         starting_modes: typing.Any = (max_modes, max_modes)
